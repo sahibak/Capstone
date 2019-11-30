7477fab970a8bd52fa64180e8fc089cfbb3ae2f0
Sahiba K <khanna.sahiba@gmail.com>
2019-11-30 15:21:59 -0500

    recipeCard basic functionality done

--- server/data/recipesData.js
+++ server/data/recipesData.js
@@ -1,9 +1,13 @@
+// let img = require("../assets/pic.jpg")
+
 module.exports = [
     {
         "name": "Lasgna",
         "type":"Italian",
         "category": "Vegetarian",
         "feeds":2,
+        "image":"https://image.shutterstock.com/image-photo/healthy-food-clean-eating-selection-260nw-722718097.jpg",
+        "recipe": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.","Vestibulum pharetra purus vulputate leo ornare iaculis."],
         "ingredients":[
             {
                 "name":"eggplant",
@@ -12,6 +16,26 @@ module.exports = [
             },
             {
                 "name":"pasta",
+                "qty":"1",
+                "unit":"box" 
+            },
+        ]
+    },
+    {
+        "name": "Curry",
+        "type":"Indian",
+        "category": "Vegan",
+        "feeds":5,
+        "image":"https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "recipe": "dfjhsfksjhfskfhskfhdskjfhsdjfnfkjsdfns",
+        "ingredients":[
+            {
+                "name":"potato",
+                "qty":"1",
+                "unit":"pound" 
+            },
+            {
+                "name":"rice",
                 "qty":"1",
                 "unit":"box" 
             },
