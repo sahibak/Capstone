041e870d9ebee11c7e982fb0aa6a9a776627eb43
Sahiba K <khanna.sahiba@gmail.com>
2019-12-05 02:21:08 -0500

    footer adjusted

--- client/src/Components/footer/footer.js
+++ client/src/Components/footer/footer.js
@@ -5,7 +5,7 @@ import "./style.scss";
 export default class Footer extends React.Component{
     render(){
         return(
-            <footer className="page-footer container body red footer-links">
+            <footer className="page-footer body red footer-links">
                 {/* <article className="footer-links black"> */}
                     <Link to="/shoppingcart"><i className="material-icons prefix">shopping_cart</i></Link>
                     <Link to="/recipebook"><i className="material-icons prefix">menu_book</i></Link>  
